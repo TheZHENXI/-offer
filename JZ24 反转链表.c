cbd6707994eb58f1a857b2cfeea984a842569a62
TheZHENXI <111845931+TheZHENXI@users.noreply.github.com>
2022-10-03 23:57:30 +0800

    双指针

---
+++ JZ24 反转链表.c
@@ -0,0 +1,39 @@
+/*
+ * @Author: error: git config user.name && git config user.email & please set dead value or install git
+ * @Date: 2022-10-03 21:55:12
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-10-03 21:56:39
+ * @FilePath: /CCODE/剑指offer/JZ24 反转链表.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/**
+ * struct ListNode {
+ *	int val;
+ *	struct ListNode *next;
+ * };
+ */
+
+/**
+ * 
+ * @param pHead ListNode类 
+ * @return ListNode类
+ */
+struct ListNode* ReverseList(struct ListNode* pHead ) 
+{
+    if(pHead == NULL)
+    {
+        return NULL;
+    }
+    struct ListNode* tmp = NULL;
+    struct ListNode* cur = pHead;
+    struct ListNode* curPre = NULL;
+    while(cur != NULL)
+    {
+        tmp = cur->next;
+        cur->next = curPre;
+        curPre = cur;
+        cur = tmp;
+    }
+    return curPre;
+    // write code here
+}
